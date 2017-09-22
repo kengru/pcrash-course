bf13897d1f1a196e096f8e75a998f94ccfe103fb
Kendry Grullon <kgrullon@vivial.net>
2017-09-22 13:21:57 -0400

    Added the whitespace and apostrophe files.

---
+++ chapter_two/apostrophe.py
@@ -0,0 +1,4 @@
+# Has two kinds of quote for strings.
+
+message = "One of Python's strengths"
+print(message)
---
+++ chapter_two/whitespace.py
@@ -0,0 +1,9 @@
+# Whitespace and methods of strings.
+
+print('Languages:\n\tPython\n\tC\n\tJavaScrpt')
+
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
