410ef471498e799b65f1967bf8e152d0ca867fc6
Kendry Grullon <kgrullon@vivial.net>
2017-09-22 14:18:10 -0400

    Created folders on chapters for the exercises.

---
+++ chapter_003/bicycles.py
@@ -0,0 +1,8 @@
+# Introducing lists.
+
+bicycle = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycle[0].title())
+print(bicycle[-1].title()) # Gets last item.
+
+message = 'My first bicycle was a ' + bicycle[1].title() + '.'
+print(message)
