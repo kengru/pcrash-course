613471609d1e921486c345ad3f02665bcd1ec213
Kendry Grullon <kengrullon@gmail.com>
2017-09-24 19:41:39 -0400

    Created some exercises on chapter 4.

---
+++ chapter_004/exercises/animals.py
@@ -0,0 +1,8 @@
+# Exercise 4-2. Printing items in a list.
+
+animals = ['dog', 'cat', 'lion']
+
+for animal in animals:
+    print('A ' + animal + ' would make a great pet.')
+
+print('I love them all!')
---
+++ chapter_004/magicians.py
@@ -0,0 +1,8 @@
+# Going through a list with a for loop, printing values.
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ', that was a great trick!')
+    print("I can't wait to see you next trick, " + magician.title() + "!\n")
+
+print('Thank you, everyone.')
