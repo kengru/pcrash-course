8f0b3865c3b09158b83f26cd2b0b95eb452c86d5
Kendry Grullon <kengrullon@gmail.com>
2017-09-25 16:22:36 -0400

    Created the directory for chapter 5 and a file with it.

---
+++ chapter_005/cars.py
@@ -0,0 +1,9 @@
+# Using If to check some conditions.
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
